74becee82280b2c0125e45313c1545c803c64fc9
Ron Dor <reichmanrd@gmail.com>
2024-11-29 21:45:22 +0200

    KeywordsDetector.java #1 TRY

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,73 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        int sentencesLength = sentences.length;
+        int keywordsLength = keywords.length;
+        for (int i = 0; i < sentencesLength; i++) {
+            String currentSentence = sentences[i];
+            for (int j = 0; j < keywordsLength; j++) {
+                String currentKeyWord = keywords[j];
+                boolean keyAtSentence = contains(currentSentence, currentKeyWord);
+                if (keyAtSentence) {
+                    System.out.println(currentSentence);
+                    break;
+                }
+            }
+        }
+    }
+
+    /** If str1 contains str2, returns true; otherwise returns false. */
+    public static boolean contains(String str1, String str2) {
+        // Case: Input Error.
+        if (str1 == null || str2 == null) {
+            return false;
+        } 
+        // Case: Empty String.
+        if (str2.equals("")) {
+            return true;
+        }
+        // Case: Str1 cannot contain Str2 as it's smaller.
+        if (str2.length() > str1.length()) {
+            return false;
+        }
+        // Logic: More comfortable dealing with lower cases.
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        // Logic: More comfortable dealing without spaces.
+        // str1 = removeSpaces(str1);
+        // str2 = removeSpaces(str2);
+        // Logic: Prevent Exceedance.
+        int iterateLimit = str1.length() - str2.length();
+        for (int i = 0; i <= iterateLimit; i++) {
+            String subString = str1.substring(i, i+str2.length());
+            if(subString.equals(str2)) {
+                return true;
+            }
+        }
+        return false;
+    }    
+
+    /** Returns the lowercase version of the given string. */
+    public static String lowerCase(String str) {
+        // Case: Input Error.
+        if (str == null) {
+            return null;
+        } 
+        // Case: Empty String.
+        if (str == "") {
+            return "";
+        }
+        String lowerString = "";
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char currentChar = charArray[i];
+            // Example: 'K' in 75, 'k' in 107.
+            if (currentChar >= 'A' && currentChar <= 'Z') {
+                lowerString += (char) (currentChar+32);
+            } else {
+                lowerString += currentChar;
+            }
+        }
+        return lowerString;
     }
 }
