a0b84e4dd9af83eedb9d817c6549c93fc52b89cb
Ron Dor <reichmanrd@gmail.com>
2024-11-29 20:28:32 +0200

    Primes.java #3 TRY

--- Primes.java
+++ Primes.java
@@ -13,11 +13,13 @@ public class Primes {
         }
         for (int i = 0; i <= Math.sqrt(nMax); i++) {
             if (numArray[i] == true) {
+                // Example: If i=3, turn 9,12,15,etc into F.
                 for (int j = i*i; j <= nMax; j+=i) {
                     numArray[j] = false;
                 }
             }
         }
+        System.out.println("Prime numbers up to " + nMax + ":");
         int countPrime = 0;
         for (int i = 0; i < numArray.length; i++) {
             if (numArray[i] == true) {
@@ -25,10 +27,8 @@ public class Primes {
                 countPrime++;
             }
         }
+        // They print % as Int, it appears.
         int pctCountInt = (int) (((double) countPrime / nMax) * 100);
-        System.out.println(String.format(
-            "There are %d primes between 2 and %d (%d%% are primes)", 
-            countPrime, nMax, pctCountInt
-        ));
-        }
+        System.out.println("There are " + countPrime + " primes between 2 and " + nMax + " (" + pctCountInt + "% are primes)");
+    }
 }
